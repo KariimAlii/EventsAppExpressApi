9d05573ee68e72db44986f0a6d7a25e9b6f8b206
Karim Ali <101140331+KariimAlii@users.noreply.github.com>
2025-10-25 04:09:11 +0300

    adjusted events json file 4

--- data/util.js
+++ data/util.js
@@ -14,3 +14,22 @@ async function readData() {
 async function writeData(data) {
     await fs.writeFile(path, JSON.stringify(data));
 }
+const fs = require('node:fs/promises');
+const path = '/tmp/events.json';
+
+async function readData() {
+    try {
+        const data = await fs.readFile(path, 'utf8');
+        return JSON.parse(data);
+    } catch {
+        // If file does not exist, return empty array
+        return [];
+    }
+}
+
+async function writeData(data) {
+    await fs.writeFile(path, JSON.stringify(data));
+}
+
+exports.readData = readData;
+exports.writeData = writeData;
